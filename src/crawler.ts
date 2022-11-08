9ec74d85ec3d3c4bb6da2bec32e2daf55e28d68d
Steve <ycsteven.tsao@gmail.com>
2022-11-08 20:17:47 +0800

    add:super agent

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,1 +1,9 @@
-console.log('hello crawler');
+class Crawler {
+  private secret = 'secretKey';
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  constructor() {
+    console.log('crawler');
+  }
+}
+
+const crawler = new Crawler();
