d01a6629c7943d3f7f804f03caa555e75be9c83c
Steve <ycsteven.tsao@gmail.com>
2022-11-08 21:04:35 +0800

    feat:get html text

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,8 +1,16 @@
+import superagent from 'superagent';
 class Crawler {
   private secret = 'secretKey';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private rawHtml = '';
+
+  async getRawHtml() {
+    const result = await superagent.get(this.url);
+    console.log(result.text);
+  }
+
   constructor() {
-    console.log('crawler');
+    this.getRawHtml();
   }
 }
 
