690b030498b87ee493f48e10b0f7b1c45c215cfd
Steve <ycsteven.tsao@gmail.com>
2022-11-09 20:53:25 +0800

    feat:build data

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,12 +1,38 @@
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string;
+  count: number;
+}
+
 class Crawler {
   private secret = 'secretKey';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-  private rawHtml = '';
+
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseItems = $('.course-item');
+    const courseInfos: Course[] = [];
+
+    courseItems.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+      courseInfos.push({ title, count });
+      console.log(title, count);
+    });
+
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+    console.log(result);
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    console.log(result.text);
+    this.getCourseInfo(result.text);
   }
 
   constructor() {
